825c37929c1659fb0bf348d83487c4532177612e
Sydney Magee <mageesj@student2.cs.appstate.edu>
2021-08-27 13:00:11 -0400

    added projects

---
+++ Program3/SydneyMageeP3.java
@@ -0,0 +1,85 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SydneyMageeP3
+{
+    private final static int[][] STATE_TABLE = {
+        {3, 1, 4}, //0 ACCEPT
+        {0, 2, 4}, //1
+        {1, 3, 4}, //2
+        {2, 0, 4}, //3 
+        {4, 4, 4}, //4 TRAP
+    };
+    
+    private BufferedReader in;
+
+    public SydneyMageeP3()
+    {
+        in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void run() throws IOException
+    {
+        char ch;
+        int state;
+        for (;;)
+        {
+            System.out.print("Enter your string: ");
+            ch = (char) in.read();
+
+            // set state to start state
+            state = 0;
+            while (ch != '\n' && ch != '\r')
+            {
+                state = STATE_TABLE[state][charToColumn(ch)];
+                ch = (char) in.read();
+            }
+            if (ch == '\r')
+            {
+                in.readLine();
+            }
+            // determine whether to accept or reject
+            if (state == 0)
+            {
+                System.out.println("Accept\n");
+            }
+            else
+            {
+                System.out.println("Reject\n");
+            }
+        }
+    }
+
+    public int charToColumn(char ch)
+    {
+        // column 2 is for some unexpected character
+        int column = 2;
+
+        switch (ch)
+        {
+            case 'a':
+                column = 0;
+                break;
+            case 'b':
+                column = 1;
+                break;
+        }
+        return column;
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            SydneyMageeP3 fsm = new SydneyMageeP3();
+            fsm.run();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
---
+++ SydneyMagee5/SydneyMagee5.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class SydneyMagee5 {
+    private final static int[][] STATE_TABLE = {
+        { 1, 2, 3, 4 },
+        { 1, 3, 2, 4 },
+        { 3, 2, 1, 4 },
+        { 2, 1, 3, 4 },
+        { 4, 4, 4, 4 },
+    };
+
+    private BufferedReader in;
+
+    public SydneyMagee5() {
+        in = new BufferedReader(
+                 new InputStreamReader(System.in));
+    }
+
+    public void run() throws IOException {
+        char ch;
+        int  state;
+        for (;;) {
+            System.out.print("Enter your string: ");
+            ch    = (char) in.read();
+
+            // set state to start state
+            state = 0;   
+                   
+            while (ch != '\n') {
+               state = STATE_TABLE[state][charToColumn(ch)];
+               ch    = (char) in.read();
+            }
+
+            // determine whether to accept or reject
+            if (state == 1){ 
+                System.out.println("1\n");
+            } 
+            else if (state == 2){
+		        System.out.println("5\n");
+	        }   
+            else if (state == 3){
+                System.out.println("8\n");
+            }
+            else{
+                System.out.println("TRAP\n");
+            }
+        }
+    }
+    public int charToColumn(char ch) {
+        // column 2 is for some unexpected character
+        int column = 3;
+
+        switch( ch ) {
+        case '1':
+            column = 0;
+            break;
+
+        case '5':
+            column = 1;
+            break;
+        
+        case '8':
+            column = 2;
+            break;
+        
+        }
+
+        return column;
+    }
+
+    public static void main(String[] args) {
+        try {
+            SydneyMagee5 fsm = new SydneyMagee5();
+            fsm.run();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
